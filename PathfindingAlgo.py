ca4cd3b8e5d1435f24a02b2fc80183c87955095f
ParthShinde2004 <69662762+ParthShinde2004@users.noreply.github.com>
2021-09-11 12:13:32 +0800

    Update PathfindingAlgo.py

--- PathfindingAlgo.py
+++ PathfindingAlgo.py
@@ -1,3 +1,13 @@
+"""
+This A* Pathfinding algorithm that finds the closest path between 2 points.
+
+Press LEFT CLICK to select start and end point.
+Then LEFT CLICK to build a barrier on the board.
+Right Click to remove objects
+Press SPACE to run the algorithm.
+Press C to clear the board.
+"""
+
 import pygame
 import math
 from queue import PriorityQueue
